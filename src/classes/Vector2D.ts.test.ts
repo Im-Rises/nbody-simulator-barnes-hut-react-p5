b42cf08db109215b6221fae96b27b3bc2bd82bf4
Im-Rises <quentin-morel88@hotmail.com>
2023-09-04 02:00:09 -0400

    wip

--- src/classes/Vector2D.ts.test.ts
+++ src/classes/Vector2D.ts.test.ts
@@ -8,17 +8,37 @@ test('Vector2D', () => {
 })
 
 test('Vector2D.add', () => {
-    // const v1 = new Vector2D(1, 2)
-    // const v2 = new Vector2D(3, 4)
-    // const v3 = v1.add(v2)
-    // expect(v3.x).toBe(4)
-    // expect(v3.y).toBe(6)
+    const v1 = new Vector2D(1, 2)
+    const v2 = new Vector2D(3, 4)
+    v1.add(v2)
+    expect(v1.x).toBe(4)
+    expect(v1.y).toBe(6)
 })
 
 test('Vector2D.sub', () => {
-    // const v1 = new Vector2D(1, 2)
-    // const v2 = new Vector2D(3, 4)
-    // const v3 = v1.sub(v2)
-    // expect(v3.x).toBe(-2)
-    // expect(v3.y).toBe(-2)
+    const v1 = new Vector2D(1, 2)
+    const v2 = new Vector2D(3, 4)
+    v1.sub(v2)
+    expect(v1.x).toBe(-2)
+    expect(v1.y).toBe(-2)
+})
+
+test('Vector2D.mult', () => {
+    const v = new Vector2D(1, 2)
+    v.mult(2)
+    expect(v.x).toBe(2)
+    expect(v.y).toBe(4)
+})
+
+test('Vector2D.div', () => {
+    const v = new Vector2D(1, 2)
+    v.div(2)
+    expect(v.x).toBe(0.5)
+    expect(v.y).toBe(1)
+})
+
+test('Vector2D.dist', () => {
+    const v1 = new Vector2D(1, 2)
+    const v2 = new Vector2D(3, 4)
+    expect(v1.dist(v2)).toBe(2.8284271247461903)
 })
