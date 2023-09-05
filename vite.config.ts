c280790635a7b8c63952b492d12a3cc5ad35856e
Im-Rises <quentin-morel88@hotmail.com>
2023-09-05 00:31:11 -0400

    Adding d.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
     build: {
         target: 'esnext', // Ensure compatibility with modern browsers
         lib: {
