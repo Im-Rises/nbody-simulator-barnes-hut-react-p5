3c156acbd298be1168f11942b34d0c3df016bacc
Im-Rises <quentin-morel88@hotmail.com>
2023-09-04 23:59:46 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,23 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // // Build react packages
-    // build: {
-    //     lib: {
-    //         entry: 'src/components/NbodySimulatorBarnesHut.tsx',
-    //         name: 'NbodySimulatorBarnesHut',
-    //         fileName: (format) => `NbodySimulatorBarnesHut.${format}.js`,
-    //     },
-    // }
+    build: {
+        target: 'esnext', // Ensure compatibility with modern browsers
+        lib: {
+            entry: 'src/components/NbodySimulatorBarnesHut.tsx', // Entry point to your React component
+            name: 'NbodySimulatorBarnesHut', // The name of your component
+            formats: ['es'], // Output format (ES module)
+        },
+        rollupOptions: {
+            // Externalize dependencies if needed
+            external: ['react', 'react-dom', 'react-p5'],
+            output: {
+                dir: 'dist',
+                entryFileNames: 'NbodySimulatorBarnesHut.js',
+                // chunkFileNames: 'NbodySimulatorBarnesHut.js',
+                // sourcemap: true,
+                // compact: false,
+            },
+        },
+    },
 })
