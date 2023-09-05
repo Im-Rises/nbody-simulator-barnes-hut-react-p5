b627bb5273a99d40810a8f1fa126980b2b46969b
Im-Rises <quentin-morel88@hotmail.com>
2023-09-04 21:18:59 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // // Build react packages
+    // build: {
+    //     lib: {
+    //         entry: 'src/components/NbodySimulatorBarnesHut.tsx',
+    //         name: 'NbodySimulatorBarnesHut',
+    //         fileName: (format) => `NbodySimulatorBarnesHut.${format}.js`,
+    //     },
+    // }
 })
